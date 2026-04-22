0fa17122097227acebad0f02b12c89b92fdf3bbb
Rezetyan <kamiomisuzu46@gmail.com>
2026-04-22 23:56:29 +0800

    chore: ignore and untrack local dev-only files

--- test_regex.py
+++
@@ -1,5 +0,0 @@
-import re
-with open('api_response.json', 'r', encoding='utf-8') as f:
-    body = f.read()
-post_ids = re.findall(r'"postId"\s*:\s*"?(\d{4,9})', body)
-print('Found:', set(post_ids))
